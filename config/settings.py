421f1551bdda29f540a6ca117222c2d86f46aae9
mszahan <mszahan1@gmail.com>
2022-05-18 22:08:22 +0600

    added login log out and signup functionality
    The basic and default login logout function though login is not working

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# this link is not working at all
+LOGIN_REDIRECT_URL = 'home' 
+
+LOGOUT_REDIRECT_URL = 'home'
